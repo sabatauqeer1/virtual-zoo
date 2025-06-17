dbdbcfcfde5936551221dd0afa57ac04e4e46b17
Saba Tauqeer <sabatauqeer24@gmail.com>
2025-06-17 05:35:36 +0500

    added new tts feature and replaced image by video , title and name css updated

--- client/src/Components/assets/virtualzoo.js
+++ client/src/Components/assets/virtualzoo.js
@@ -8,6 +8,9 @@ import { useRef } from "react";
 export const VirtualZoo = () => {
   const navigate = useNavigate();
   const hasSlideshowStarted = useRef(false);
+  const [isSpeaking, setisSpeaking] = useState(!true);
+  const [IsReloading, setIsReloading] = useState(!true);
+  const synth = window.speechSynthesis;
 
   const [animal, setanimal] = useState(null);
 
@@ -20,12 +23,19 @@ export const VirtualZoo = () => {
       );
 
       setanimal(response.data);
+      window.onbeforeunload = () => {
+        synth.cancel();
+        setIsReloading(true);
+      };
     };
 
     animalInfo();
   }, [animalId]);
 
   const slideshowForward = () => {
+    synth.cancel();
+    setisSpeaking(!true);
+
     const values = Object.values(data);
     const ids = values.map((item) => String(item.id));
     const currentId = String(animalId);
@@ -39,6 +49,9 @@ export const VirtualZoo = () => {
   };
 
   const slideshowBack = () => {
+    synth.cancel();
+    setisSpeaking(!true);
+
     const values = Object.values(data);
     const ids = values.map((item) => String(item.id));
     const currentId = String(animalId);
@@ -51,18 +64,50 @@ export const VirtualZoo = () => {
       hasSlideshowStarted.current = true;
     }
   };
+  const speakButtonHandler = () => {
+    try {
+      const speakThis =
+        "name: " +
+        animal.name +
+        ", " +
+        "type: " +
+        animal.types +
+        ", " +
+        "description: " +
+        animal.description;
+      console.log(speakThis);
+
+      const utterThis = new SpeechSynthesisUtterance(speakThis);
+      synth.speak(utterThis);
+      setisSpeaking(true);
+    } catch (error) {}
+    if (isSpeaking === true) {
+      synth.cancel();
+      setisSpeaking(!true);
+    }
+  };
 
   return (
     <div>
       {animal ? (
         <div>
           <h1 id="title">VIRTUAL ZOO</h1>
+          <h2 id="animalName">{animal.name}</h2>
           <div id="animalImgDiv">
-            <img src={`/${animalId}.jpg`} alt={animal.name} id="animalImg" />
+            <video
+              id="animalImg"
+              src={`/${animalId}.mp4`}
+              autoPlay
+              loop
+              muted
+              playsInline
+              controlsList="nodownload nofullscreen noremoteplayback"
+              controls
+            />
           </div>
+
           <div id="animalInfoDiv">
             <div id="animalInfo">
-              <h2>Name:{animal.name}</h2>
               <h2>Type:{animal.types}</h2>
               <h2>Description:{animal.description}</h2>
             </div>
@@ -72,6 +117,9 @@ export const VirtualZoo = () => {
           </div>
           <div id="backSlideShowButton">
             <button onClick={slideshowBack}>back</button>
+          </div>
+          <div id="speakButton">
+            <button onClick={speakButtonHandler}> Speak</button>
           </div>
         </div>
       ) : (
