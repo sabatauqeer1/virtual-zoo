5fa90864f24e044d6c5c756813716b3722b71cba
sabatauqeer1 <sabatauqeer860@gmail.com>
2025-07-05 01:38:23 +0500

    Update virtualzoo.js

--- client/src/Components/assets/virtualzoo.js
+++ client/src/Components/assets/virtualzoo.js
@@ -85,10 +85,10 @@ export const VirtualZoo = () => {
     }
   };
 
-  return (
-    <div>
+ return (
+  <div>
     {animal ? (
-      <div>
+      <>
         <h1 id="title">VIRTUAL ZOO</h1>
         <h2 id="animalName">{animal.name}</h2>
         <div id="animalImgDiv">
@@ -103,30 +103,32 @@ export const VirtualZoo = () => {
             controls
           />
         </div>
-      </div>
-    ) : null}
-  </div>
-          <div id="animalInfoDiv">
-            <div id="animalInfo">
-              <h2>Type:{animal.types}</h2>
-              <h2>Description:{animal.description}</h2>
-            </div>
-          </div>
-          <div id="forwardSlideShowButton">
-            <button onClick={slideshowForward}>forward</button>
-          </div>
-          <div id="backSlideShowButton">
-            <button onClick={slideshowBack}>back</button>
-          </div>
-          <div id="speakButton">
-            <button onClick={speakButtonHandler}> Speak</button>
+
+        <div id="animalInfoDiv">
+          <div id="animalInfo">
+            <h2>Type: {animal.types}</h2>
+            <h2>Description: {animal.description}</h2>
           </div>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+
+        <div id="forwardSlideShowButton">
+          <button onClick={slideshowForward}>forward</button>
+        </div>
+
+        <div id="backSlideShowButton">
+          <button onClick={slideshowBack}>back</button>
+        </div>
+
+        <div id="speakButton">
+          <button onClick={speakButtonHandler}>Speak</button>
+        </div>
+      </>
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
+);
+
 };
 
 export default VirtualZoo;
