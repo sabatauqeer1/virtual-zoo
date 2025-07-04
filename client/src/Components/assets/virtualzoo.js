52b2b7359ddad67869f9abd74ce0088e7cbcfee4
Saba Tauqeer <sabatauqeer24@gmail.com>
2025-07-05 01:25:55 +0500

    updated react

--- client/src/Components/assets/virtualzoo.js
+++ client/src/Components/assets/virtualzoo.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
